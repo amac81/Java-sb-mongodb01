a45373e0e1bbafde00046b11e623a053a57199df
amac81 <arnaldocanelas@gmail.com>
2023-07-26 17:01:30 +0100

    project create

---
+++ src/main/java/pt/bitclinic/Javasbmongodb01/JavaSbMongodb01Application.java
@@ -0,0 +1,13 @@
+package pt.bitclinic.Javasbmongodb01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSbMongodb01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSbMongodb01Application.class, args);
+	}
+
+}
---
+++ src/test/java/pt/bitclinic/Javasbmongodb01/JavaSbMongodb01ApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.bitclinic.Javasbmongodb01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSbMongodb01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
