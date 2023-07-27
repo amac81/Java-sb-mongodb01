ec7cdb0eabc737afa7379dd4b4ecaa6258261c41
amac81 <arnaldocanelas@gmail.com>
2023-07-27 17:22:54 +0100

    User REST - find all

---
+++ src/main/java/pt/bitclinic/Javasbmongodb01/resources/UserResource.java
@@ -0,0 +1,31 @@
+package pt.bitclinic.Javasbmongodb01.resources;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pt.bitclinic.Javasbmongodb01.domain.User;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+
+	@GetMapping // or @RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+		User user1 = new User("1", "Mary Jane", "mary@gmail.com");
+		User user2 = new User("2", "Bob Brown", "bob@gmail.com");
+		User user3 = new User("3", "Chris Charlie", "chris@gmail.com");
+
+		List <User> users = new ArrayList<> ();	
+		users.addAll(Arrays.asList(user1, user2, user3));
+		
+		return ResponseEntity.ok().body(users);
+	}
+}
+
