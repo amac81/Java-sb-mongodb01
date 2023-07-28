6f8f36533ba6d5dcce242b2573a1719e0ce1b568
amac81 <arnaldocanelas@gmail.com>
2023-07-28 18:40:50 +0100

    GET post by Id

---
+++ src/main/java/pt/bitclinic/Javasbmongodb01/services/PostService.java
@@ -0,0 +1,26 @@
+package pt.bitclinic.Javasbmongodb01.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import pt.bitclinic.Javasbmongodb01.domain.Post;
+import pt.bitclinic.Javasbmongodb01.repositories.PostRepository;
+import pt.bitclinic.Javasbmongodb01.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	@Transactional(readOnly = true)
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Post " + id + " not found."));
+	}
+
+
+}
